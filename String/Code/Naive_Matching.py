b843a392713bd4eaf83ba8dd2508884a304f9e03
Donrinho <donrinho1111@gmail.com>
2018-08-26 20:34:22 +0800

    Create Naive_Matching.py

---
+++ String/Code/Naive_Matching.py
@@ -0,0 +1,11 @@
+def naive_matching(t, p):
+    m, n = len(p), len(t)
+    i, j = 0, 0
+    while i < m and j < n:
+        if p[i] == t[j]:
+            i, j = i + 1, j + 1
+        else:
+            i, j = 0, j - i + 1
+    if i == m:
+        return j - i
+    return -1
